661681828ba2c5ef590dcdfbbffbb9c6a17f663f
Leonardo Isaias <leoisaias135@gmail.com>
2025-02-15 04:17:06 -0300

    Código mais limpo e organizado, utilização de boas práticas de extração de métodos, criação de url fixa para o controller, retornar os dados de uma única série

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -32,4 +32,6 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
 
     @Query("Select e from Serie s JOIN s.episodios e WHERE s = :serie AND YEAR(e.dataLancamento) >= :anoLancamento")
     List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
+
+    List<Serie> findTop5ByOrderByEpisodiosDataLancamentoDesc();
 }
